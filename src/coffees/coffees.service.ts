f75ee3b7c12b14be1af557bf90cc93e7f128d4c1
Deepak Bhari <deeebeee3@hotmail.com>
2022-11-30 16:58:33 +0000

    Send user friendly error messages

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  // HttpException,
+  // HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -17,7 +22,17 @@ export class CoffeesService {
   }
 
   findOne(id: string) {
-    return this.coffees.find((item) => item.id === +id);
+    /* Using nestjs built in exception layer this random error will be caught (500 Internal Server error) */
+    /* throw 'A random error'; */
+
+    const coffee = this.coffees.find((item) => item.id === +id);
+
+    if (!coffee) {
+      /* throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND); */
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+
+    return coffee;
   }
 
   create(createCoffeeDto: any) {
