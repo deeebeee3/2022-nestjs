e163a61cc99031bc58277af0779c323129103782
Deepak Bhari <deeebeee3@hotmail.com>
2022-12-01 16:05:42 +0000

    Relations ARE NOT eagerly loaded by default

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -18,14 +18,20 @@ export class CoffeesService {
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({
+      relations: ['flavours'],
+    });
   }
 
   async findOne(id: number) {
     /* Using nestjs built in exception layer this random error will be caught (500 Internal Server error) */
     /* throw 'A random error'; */
 
-    const coffee = await this.coffeeRepository.findOne({ where: { id: id } });
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id: id },
+      relations: ['flavours'],
+    });
+
     if (!coffee) {
       /* throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND); */
       throw new NotFoundException(`Coffee #${id} not found`);
