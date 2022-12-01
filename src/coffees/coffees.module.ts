605175b5040c9ffb70c46690e4a37c0ac016fb89
Deepak Bhari <deeebeee3@hotmail.com>
2022-12-01 15:08:36 +0000

    RELATIONS: Finally, register Flavour entity with TypeORM

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
+import { Flavour } from './entities/flavour.entity';
 
 @Module({
   /* register entity using forFeature func from TypeOrmModule */
-  imports: [TypeOrmModule.forFeature([Coffee])],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavour])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
