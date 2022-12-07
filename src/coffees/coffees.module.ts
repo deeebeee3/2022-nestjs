6feb8f9f056af2ae4c92b4ca794d0dc68c79f11a
Deee Beee 3 <deeebeee3@hotmail.com>
2022-12-07 00:29:07 +0000

    Factory Provider - A more releastic example of it

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
@@ -7,13 +7,21 @@ import { Flavour } from './entities/flavour.entity';
 import { Event } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
-class MockCoffeesService {}
+//class MockCoffeesService {}
 
 /* imagine this is abstract class, and depending on the dev env, 
 the below two classes are different implementations of it */
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductionConfigService {}
+// class ConfigService {}
+// class DevelopmentConfigService {}
+// class ProductionConfigService {}
+
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    /* ... do something ... */
+    return ['buddy brew', 'nescafe'];
+  }
+}
 
 @Module({
   /* register entity using forFeature func from TypeOrmModule */
@@ -21,9 +29,16 @@ class ProductionConfigService {}
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    CoffeeBrandsFactory,
     // { provide: CoffeesService, useValue: new MockCoffeesService() },
     // { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
-    { provide: COFFEE_BRANDS, useFactory: () => ['buddy brew', 'nescafe'] },
+    // { provide: COFFEE_BRANDS, useFactory: () => ['buddy brew', 'nescafe'] },
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
+    },
     // {
     //   provide: ConfigService,
     //   useClass:
